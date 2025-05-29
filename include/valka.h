c6b9c29f36a3f9f744fe3fbda72da0d17c28bfce
matteoepitech <matteo.tutti@epitech.eu>
2025-05-29 19:20:46 +0200

    feat(llvm cast): add llvm prototype for casting. Not implemented yet.

--- include/valka.h
+++ include/valka.h
@@ -60,6 +60,7 @@ uint8_t llvm_string(ast_node_t *node, FILE *f, char *dest);
 uint8_t llvm_if(ast_node_t *node, FILE *f);
 uint8_t llvm_condition(ast_node_t *node, FILE *f, char *dest);
 uint8_t llvm_assign(ast_node_t *node, FILE *f);
+uint8_t llvm_cast(ast_node_t *node, FILE *f, char *dest);
 
 /*
  * Folder : utils/
---
+++ src/core/llvm/dispatch/llvm_cast.c
@@ -0,0 +1,22 @@
+/*
+** VALKA PROJECT, 2025
+** src/core/llvm/dispatch/llvm_cast
+** File description:
+** LLVM for cast declaration
+*/
+
+#include "valka.h"
+
+/**
+ * @brief LLVM cast declaration.
+ *
+ * @param node           The AST node
+ * @param f              The FILE to write in
+ *
+ * @return Everything worked?
+ */
+uint8_t
+llvm_cast(ast_node_t *node, FILE *f, char *dest)
+{
+    return OK_OUTPUT;
+}
